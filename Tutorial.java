e697c0c395fd5b3eb1acb62c8ee8d99571c4da35
Tutorials99 <almajed007@msn.com>
2018-09-10 01:42:29 -0400

    Uploading

---
+++ Tutorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class Tutorial {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+        System.out.println("your name is: " + name);
+    }
+}
