df4954307036f781c00e1fbd435d014904f9c5ac
Tutorials99 <almajed007@msn.com>
2018-09-10 01:45:23 -0400

    Uploading

--- Tutorial.java
+++ Tutorial.java
@@ -7,9 +7,9 @@ public class Tutorial {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.next();
-        System.out.println("your name is: " + name);
+        Scanner scanner = new Scanner(System.in); //تعريف السكانر المسؤول عن اخذ الانبوت من اليوزر
+        System.out.println("Enter your name: "); //سؤال اليوزر عن اسمه
+        String name = scanner.next();//اخذ الانبوت وتخزينه في فاريبل
+        System.out.println("your name is: " + name); //طباعة الاسم لليوزر
     }
 }
